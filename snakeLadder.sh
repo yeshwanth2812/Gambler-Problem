4c91f3b101dab07c7ff7fce10b067a1eb8e044b8
yeshwanth2812 <yesh281297@gmail.com>
2020-06-11 10:25:12 +0530

    Repeating till the Player reaches the winning position 100

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,19 +1,24 @@
 playerPos=0
 rollDie() {
   dieValue=$(( $((RANDOM % 6)) + 1 ))
+  echo "Die value ${dieValue}"
 }
-option=$((RANDOM % 3))
-case $option in
-  0)  echo "No Play" ;;
-  1)  rollDie
-      playerPos=$((playerPos + dieValue))
-      echo 'player got a ladder'
-      echo "player position is ${playerPos}";;
-  2)  rollDie
-      playerPos=$((playerPos - dieValue))
-      if [ $playerPos -lt 0 ]; then
-        playerPos=0
-      fi
-      echo 'player got a snake'
-      echo "player position is ${playerPos}" ;;
-esac
+while [ $playerPos -ne 100 ]
+do
+  option=$((RANDOM % 3))
+  case $option in
+    0)  echo "No Play" ;;
+    1)  rollDie
+        playerPos=$((playerPos + dieValue))
+        echo 'player got a ladder'
+        echo "player position is ${playerPos}";;
+    2)  rollDie
+        playerPos=$((playerPos - dieValue))
+        if [ $playerPos -lt 0 ]; then
+          playerPos=0
+        fi
+        echo 'player got a snake'
+        echo "player position is ${playerPos}" ;;
+  esac
+done
+echo 'player won'
