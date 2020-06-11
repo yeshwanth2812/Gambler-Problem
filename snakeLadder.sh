2f896dcd2ac7702df876045f3f03374a59a299fa
yeshwanth2812 <yesh281297@gmail.com>
2020-06-11 10:16:45 +0530

    Added: Player then checks for a Option using RANDOM. They are No Play,Ladder or Snake

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,4 +1,19 @@
-player1='player1'
-start=0
-dieValue=$(( $((RANDOM % 6)) + 1 ))
-echo "Player got a number ${dieValue}"
+playerPos=0
+rollDie() {
+  dieValue=$(( $((RANDOM % 6)) + 1 ))
+}
+option=$((RANDOM % 3))
+case $option in
+  0)  echo "No Play" ;;
+  1)  rollDie
+      playerPos=$((playerPos + dieValue))
+      echo 'player got a ladder'
+      echo "player position is ${playerPos}";;
+  2)  rollDie
+      playerPos=$((playerPos - dieValue))
+      if [ $playerPos -lt 0 ]; then
+        playerPos=0
+      fi
+      echo 'player got a snake'
+      echo "player position is ${playerPos}" ;;
+esac
