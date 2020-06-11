2542fc36e875c750a4d396aed2351c94d8b0c08f
yeshwanth2812 <yesh281297@gmail.com>
2020-06-11 11:20:45 +0530

    Added: Play the game with 2 Player and report which Player won the game

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,31 +1,45 @@
-playerPos=0
+playerOne=1
+playerTwo=2
+playerOnePos=0
+playerTwoPos=0
 diceCount=0
 rollDie() {
+  playerPos=$1
   diceCount=$((diceCount + 1))
   dieValue=$(( $((RANDOM % 6)) + 1 ))
   echo "Die value ${dieValue}"
-}
-while [ $playerPos -ne 100 ]
-do
   option=$((RANDOM % 3))
   case $option in
     0)  echo "No Play" ;;
-    1)  rollDie
+    1)  echo 'player got a ladder'
         if [ $((playerPos + dieValue)) -gt 100 ]; then
           echo 'Player got a value greater than 100 so stays in the same position'
         else
           playerPos=$((playerPos + dieValue))
-          echo 'player got a ladder'
-        fi
-        echo "player position is ${playerPos}";;
-    2)  rollDie
-        playerPos=$((playerPos - dieValue))
-        if [ $playerPos -lt 0 ]; then
-          playerPos=0
-        fi
-        echo 'player got a snake'
-        echo "player position is ${playerPos}" ;;
+        fi ;;
+    2)  echo 'player got a snake'
+        if [ $((playerPos - dieValue)) -lt 0 ]; then
+          echo 'Player got a value less than 0 so stays in the same position'
+        else
+          playerPos=$((playerPos - dieValue))
+        fi ;;
   esac
+  return $playerPos
+}
+while [ $playerOnePos -ne 100 -o $playerTwoPos -ne 100 ]
+do
+  echo 'Player 1 turn'
+  rollDie $playerOnePos
+  playerOnePos=$?
+  echo "player one position ${playerOnePos}"
+  echo 'Player 2 turn'
+  rollDie $playerTwoPos
+  playerTwoPos=$?
+  echo "player two position ${playerTwoPos}"
 done
-echo 'player won'
+if [ $playerOnePos -eq 100 ]; then
+  echo 'Player 1 won'
+else
+  echo 'Player 2 won'
+fi
 echo "Total Number of times dice rolled ${diceCount}"
